73099c254f07765d95f199d81c29274b585f4fbf
sixstringstorm <mattstormmedia@gmail.com>
2017-08-23 10:45:41 -0700

    Adding books function

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,30 @@
       <h1>Vue JS & Firebase</h1>
 
     </div>
+
+    <div class="panel panel-default">
+      <div class="panel-heading">
+        <h2>Add Book</h2>
+      </div>
+      <div class="panel-body">
+        <form id="form" class="form-inline" v-on:submit.prevent="addBook">
+          <div class="form-group">
+            <label for="bookTitle">Title:</label>
+            <input type="text" id="bookTitle" class="form-control" v-model="newBook.title"/>
+          </div>
+          <div class="form-group">
+            <label for="bookAuthor">Author:</label>
+            <input type="text" id="bookAuthor" class="form-control" v-model="newBook.author"/>
+          </div>
+          <div class="form-group">
+            <label for="bookUrl">URL:</label>
+            <input type="text" id="bookUrl" class="form-control" v-model="newBook.url"/>
+          </div>
+          <input type="submit" class="btn btn-primary" value="Add Book">
+        </form>
+      </div>
+    </div>
+
     <div class="panel panel-default">
       <div class="panel-heading">
         <h3>Books List</h3>
@@ -49,6 +73,23 @@ export default {
   name: 'app',
   firebase: {
     books: booksRef
+  },
+  data () {
+    return {
+      newBook: {
+        title: '',
+        author: '',
+        url: ''
+      }
+    }
+  },
+  methods: {
+    addBook: function(){
+      booksRef.push(this.newBook);
+      this.newBook.title = '';
+      this.newBook.author = '';
+      this.newBook.url = '';
+    }
   }
 }
 </script>
